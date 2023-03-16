f78c690a423b5ac1762e0e816adf65c44e60035d
vidvidvid <weetopol@gmail.com>
2023-03-16 11:39:34 +0100

    add plausible tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import '@/assets/styles/fonts.css';
 import { ChakraProvider, useColorMode } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import React, { useEffect } from 'react';
 
 import { AppLayout } from '@/components/Layout/AppLayout';
+import { PLAUSIBLE_DATA_DOMAIN } from '@/utils/constants';
 import { globalStyles, theme } from '@/utils/theme';
 
 const ForceDarkMode: React.FC<{ children: JSX.Element }> = ({ children }) => {
@@ -24,6 +26,13 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
     <ChakraProvider resetCSS theme={theme}>
       <ForceDarkMode>
         <React.Fragment>
+          <Script
+            type="text/javascript"
+            defer
+            data-domain={PLAUSIBLE_DATA_DOMAIN}
+            data-api="/jjmahtdkrp/api/event"
+            src="/jjmahtdkrp/js/script.js"
+          />
           <Global styles={globalStyles} />
           <AppLayout>
             <Component {...pageProps} />
